c4a4f6df0829833f4d5b15cd7d4d2efb8fde9b8f
Dmitry <radiator211@mail.ru>
2022-08-06 18:37:11 +0700

    Setting TabBar, add to XIB

--- SurfSummerSchoolProject/AppDelegate.swift
+++ SurfSummerSchoolProject/AppDelegate.swift
@@ -13,46 +13,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("Приложение запущено")
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .red
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
-        return true
-    }
+       func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+           window = UIWindow(frame: UIScreen.main.bounds)
+           window?.makeKeyAndVisible()
+//           if #available(iOS 13.0, *) {
+//               window?.overrideUserInterfaceStyle = .light
+//           }
 
-    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        // Not running -> inactive
-        print("Приложение начало запуск")
-        return true
-    }
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Из Active в Inactive
-        print("Приложение приостановило действие")
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Inactive -> Active
-        print("Снова активно")
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // To background
-        print("Приложение перешло в бэкграунд")
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Background to foreground(Inactive)
-        print("Снова становится активно")
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        print("Удалено из памяти")
-    }
-    
+           runMainFlow()
 
+           return true
+       }
+
+       func runMainFlow() {
+           window?.rootViewController = TabBarConfigurator().configure()
+       }
+
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        let viewController = UIViewController()
+//        viewController.view.backgroundColor = .red
+//        window?.rootViewController = viewController
+//        window?.makeKeyAndVisible()
+//        return true
+//    }
 }
-
---
+++ SurfSummerSchoolProject/FavoriteModule/FavoriteViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoriteViewController.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Дмитрий Бессонов on 06.08.2022.
+//
+
+import UIKit
+
+final class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
